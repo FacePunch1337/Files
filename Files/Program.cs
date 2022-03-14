d4f5d4c54ae4a436d1116bbecfe4c7f68b688318
Stas Solomon <Solo_jf68@student.itstep.org>
2022-03-14 07:02:42 +0200

    Добавьте файлы проекта.

---
+++ Files/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    internal class Program
+    {
+        const String _FName = "file.txt";  
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(_FName);
+
+            sw.WriteLine("Hello File 2!");
+            sw.Close();
+                
+            try 
+            {
+                using (StreamReader sr = new StreamReader(_FName))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                    sr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
